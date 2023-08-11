49c5105fab73cf70877c707bdad9e31948551429
AsrarTamim <79744576+AsrarTamim@users.noreply.github.com>
2023-08-11 17:24:39 +0600

    Add files via upload

---
+++ leetcode/Two_sums.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      map<int,int>mp;
+      for(int i = 0; i < nums.size(); i++) mp[nums[i]] = i;
+      for(int i = 0; i < nums.size(); i++){
+        int x = target - nums[i];
+        if(mp[x] and mp[x] != i) return {i,mp[x]};
+      }  
+    return{};}   
+};
